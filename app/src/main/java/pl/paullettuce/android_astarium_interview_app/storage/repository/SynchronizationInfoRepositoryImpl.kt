aaba243aa674e9923cfe421e21ebb3b2fded3373
Paul Lettuce <paullettuce@gmail.com>
2021-01-20 18:29:16 +0100

    Create helper class for synchronization timing purpose. Set cache validity age to 24hours

--- app/src/main/java/pl/paullettuce/android_astarium_interview_app/storage/repository/SynchronizationInfoRepositoryImpl.kt
+++ app/src/main/java/pl/paullettuce/android_astarium_interview_app/storage/repository/SynchronizationInfoRepositoryImpl.kt
@@ -8,6 +8,7 @@ class SynchronizationInfoRepositoryImpl(
     private val sharedPreferences: SharedPreferences
 ): SynchronizationInfoRepository {
     private val KEY_LAST_SYNCH_TIMESTAMP = "last_synchronization_timestamp"
+    private val CACHE_VALIDITY_AGE = 24 * 60 * 60 * 1000
 
     override fun saveSynchronizationTimestampNow() {
         sharedPreferences.edit(commit = true) { putLong(KEY_LAST_SYNCH_TIMESTAMP, now()) }
@@ -16,13 +17,12 @@ class SynchronizationInfoRepositoryImpl(
     override fun isSynchronizationNeeded(): Boolean {
         val lastSynchronizationTimestamp = getLastSynchronizationTimestamp()
         if (lastSynchronizationTimestamp <= 0) return true
-        return now() - lastSynchronizationTimestamp >= oneDayAsMilliseconds()
+        return now() - lastSynchronizationTimestamp >= CACHE_VALIDITY_AGE
     }
 
     private fun getLastSynchronizationTimestamp(): Long {
         return sharedPreferences.getLong(KEY_LAST_SYNCH_TIMESTAMP, 0)
     }
 
-    private fun oneDayAsMilliseconds() = 24 * 60 * 60 * 1000
     private fun now() = System.currentTimeMillis()
 }
